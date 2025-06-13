a1d06dd4e860a2723f6c6ed9effa26208457d436
Anroux <anrouxb314@gmail.com>
2025-06-13 11:49:07 +0200

    building

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import "../app/globals.css";
+import { ProductProvider } from "./providers/productProvider";
 
 export default function RootLayout({children,}: {children: React.ReactNode;}) {
   return (
-    <html lang="en">
-      <body id="mainBody">{children}</body>
-    </html>
+    <ProductProvider>
+      <html lang="en">
+        <body id="mainBody">{children}</body>
+      </html>
+    </ProductProvider>
   );
 }
