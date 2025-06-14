f804dd4e0bf679e2ddecbd8c8385a63a94e52a56
Anroux <anrouxb314@gmail.com>
2025-06-14 12:41:32 +0200

    login page style update

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -9,102 +9,108 @@ import Header from "../../components/header/header";
 export default function Login() {
   return (
     <>
-      <Header />
-      <div
-        style={{
-          alignItems: "center",
-          justifyItems: "center",
-          justifyContent: "center",
-          display: "flex",
-        }}
-      >
-        <Form
-          name="login"
-          initialValues={{ remember: true }}
+      <div style={{ height: "100%", width: "100%", alignContent: "center" }}>
+        <div style={{ position: "fixed", top: 0, width: "inherit" }}>
+          <Header />
+        </div>
+        <div
           style={{
             alignItems: "center",
             justifyItems: "center",
             justifyContent: "center",
-            // border: "2px solid gray",
-            // borderRadius: "10px",
-            // width: "fit-content",
-            // padding: "2rem",
-            // marginTop: "3rem",
-            // minWidth: "150px",
+            display: "flex",
           }}
-          // onFinish={onFinish}
         >
-          <h1
+          <Form
+            name="login"
+            initialValues={{ remember: true }}
             style={{
-              justifyContent: "center",
               alignItems: "center",
-              fontSize: "35px",
-              padding: "1rem",
+              justifyItems: "center",
+              justifyContent: "center",
+              // border: "2px solid gray",
+              // borderRadius: "10px",
+              // width: "fit-content",
+              // padding: "2rem",
+              // marginTop: "3rem",
+              // minWidth: "150px",
             }}
+            // onFinish={onFinish}
           >
-            Login
-          </h1>
-
-          <Form.Item
-            name="email"
-            rules={[{ required: true, message: "Please input your Email!" }]}
-          >
-            <Input
-              size="large"
-              placeholder="Email"
-              prefix={<MailOutlined />}
-              style={{ width: "300px" }}
-            />
-          </Form.Item>
-
-          <Form.Item
-            name="password"
-            rules={[{ required: true, message: "Please input your Password!" }]}
-          >
-            <Input.Password
-              style={{ width: "300px" }}
-              size="large"
-              placeholder="Password"
-              prefix={<LockOutlined />}
-              iconRender={(visible) =>
-                visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
-              }
-            />
-          </Form.Item>
-          <Form.Item>
-            <Flex justify="space-between" align="center">
-              <Form.Item name="remember" valuePropName="checked" noStyle>
-                <Checkbox>Remember me</Checkbox>
-              </Form.Item>
-              <a href="">Forgot password?</a>
-            </Flex>
-          </Form.Item>
-          <Form.Item>
-            <Button
-              block
-              type="primary"
-              htmlType="submit"
-              style={{ width: "300px", fontWeight: "bold" }}
-              size="large"
+            <h1
+              style={{
+                justifyContent: "center",
+                alignItems: "center",
+                fontSize: "35px",
+                padding: "1rem",
+              }}
             >
-              Log in
-            </Button>
-          </Form.Item>
+              Login
+            </h1>
 
-          <Form.Item>
-            <Button
-              block
-              type="primary"
-              htmlType="submit"
-              style={{ width: "300px" }}
-              size="large"
+            <Form.Item
+              name="email"
+              rules={[{ required: true, message: "Please input your Email!" }]}
             >
-              <a href="/register" style={{ fontWeight: "bold" }}>
-                Register
-              </a>
-            </Button>
-          </Form.Item>
-        </Form>
+              <Input
+                size="large"
+                placeholder="Email"
+                prefix={<MailOutlined />}
+                style={{ width: "300px" }}
+              />
+            </Form.Item>
+
+            <Form.Item
+              name="password"
+              rules={[
+                { required: true, message: "Please input your Password!" },
+              ]}
+            >
+              <Input.Password
+                style={{ width: "300px" }}
+                size="large"
+                placeholder="Password"
+                prefix={<LockOutlined />}
+                iconRender={(visible) =>
+                  visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+                }
+              />
+            </Form.Item>
+            <Form.Item>
+              <Flex justify="space-between" align="center">
+                <Form.Item name="remember" valuePropName="checked" noStyle>
+                  <Checkbox>Remember me</Checkbox>
+                </Form.Item>
+                <a href="">Forgot password?</a>
+              </Flex>
+            </Form.Item>
+            <Form.Item>
+              <Button
+                block
+                type="primary"
+                htmlType="submit"
+                style={{ width: "300px", fontWeight: "bold" }}
+                size="large"
+              >
+                Log in
+              </Button>
+            </Form.Item>
+
+            <Form.Item>
+              <Button
+                block
+                type="primary"
+                htmlType="submit"
+                style={{ width: "300px" }}
+                size="large"
+              >
+                <a href="/register" style={{ fontWeight: "bold" }}>
+                  Register
+                </a>
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
       </div>
     </>
   );
